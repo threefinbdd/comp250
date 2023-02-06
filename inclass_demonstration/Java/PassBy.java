c3bb9c564bc3c2e772b5761b9b97a60643349223
threefinbdd <dave.15@wright.edu>
2023-02-05 20:54:31 -0500

    Pass By examples

---
+++ inclass_demonstration/Java/PassBy.java
@@ -0,0 +1,39 @@
+/**
+ * Sample code designed to demonstrate program's
+ * ability to manipulate code by passing parameters
+ * by Value and by Reference.
+ * 
+ * "Pass By Value" allocates (uses by creation) entirely new
+ * memory addresses when using parameters.
+ * 
+ * "Pass By Reference" uses existing memory addresses
+ * when using parameters.
+ */
+public class PassBy {
+
+    public static void passByMemoryAddress(int[] myValue) {
+        System.out.println("Inside Method: " + myValue);
+
+        myValue[0] = 100;
+    }
+
+    public static void passByValue(int myValue) {
+        myValue = 10;  // re-assigning the parameter a different value
+    }
+
+    public static void main(String[] args) {
+        int myInitValue1 = 5;
+        System.out.println("Before Pass By Value: " + myInitValue1);
+        passByValue(myInitValue1);
+        System.err.println("After PassByValue: " + myInitValue1);
+
+        int[] myInitValue2 = new int[1];
+        myInitValue2[0] = 25;
+        System.out.println("Before Pass By Value: " + myInitValue2);
+        System.out.println("Before Pass By Value: " + myInitValue2[0]);
+        passByMemoryAddress(myInitValue2);
+        System.err.println("After PassByValue: " + myInitValue2);
+        System.err.println("After PassByValue: " + myInitValue2[0]);
+
+    }
+}
