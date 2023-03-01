38438da97f368aceabb290e65cdad69baf2b38b5
threefinbdd <dave.15@wright.edu>
2023-03-01 17:20:14 -0500

    Added ArrayList Supplemental Learning

---
+++ Assignments_and_Projects/Supplemental/ListOfNames.java
@@ -0,0 +1,19 @@
+public class ListOfNames {
+    public static void main(String[] args) {
+        /*
+         * Create an ArrayList that stores String datatypes.
+         * 
+         * Add three Strings that represent:
+         *  "cat", "dog", "mouse", "cat"
+         * 
+         *  The program must perform the steps below in order:
+         *      1. Display the contents of the list
+         *      2. Remove "mouse"
+         *      3. Add "bird"
+         *      4. Add "snake"
+         *      5. Display the size of the list
+         *      6. Display the contents of the list (again)
+         *      7. Display the index of the 2 matching animals
+         */
+    }
+}
