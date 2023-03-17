b2fd8d222a1b8ea35684f8426e37061b902fe180
bddave <dave.15@wright.edu>
2023-03-16 21:09:48 -0400

    SortMyObject project scope

---
+++ Assignments_and_Projects/SortMyObject.java
@@ -0,0 +1,51 @@
+/**
+ * SortMyObject.java
+ * 
+ * Purpose:  Describing your own objects and 
+ * implementing a Comparator from a Comparable
+ * Class to sort a list of your objects through
+ * varying properties.
+ * 
+ * Instruction:
+ * Your first task is to select one of the below categories:
+ *      1) Television Shows/Series
+ *      2) Species of a Chosen Animal/Plant
+ *      3) Occupations of Computer Engineering/Science
+ *      4) Grocery Produce
+ *      5) Specifications for Computer Peripherals
+ * 
+ * Your second task is to design fields you wish to describe the 
+ * Class objects.  Below are a few options you may utilize, or 
+ * come up with your own
+ *      1)  Television Shows/Series
+ *              Name, Date Started, Date Ended, isActive (for a show that may still be airing new episodes)
+ *              Scrubs, October 2001, March 2010, False
+ *      2)  Species - Felines
+ *              Species, Minimum Lifespan (in years), Maximum Lifespan (in years), Average Lifespan (in years), isEndangered
+ *              Tiger, 8, 10, 9, True
+ *      3)  Occupations (https://roadmap.sh/backend or https://roadmap.sh/frontend)
+ *              Occupation Label, Average Annual Salary in Ohio 
+ *              Cyber Security Professional, 94,220
+ *      4)  Grocery Store:
+ *              Produce Label, Average Cost, Average Shelf Life (in days)
+ *              Oranges, 1.09, 7
+ *      5)  Specifications Sheet for Monitors:
+ *              Manufacturer, Model, Screen Size (diagonal), Cost (New) 
+ *              Benq, EX240, 24 inches, $239
+ * 
+ * After defining your fields (or using the ones provided above), create 5-10 Class objects
+ * and add them to an ArrayList of <Your Class Object>
+ * 
+ * Print out the ArrayList
+ * 
+ * Utilize a Comparator from the Comparable Class to sort the ArrayList from one field.
+ * 
+ * Print out the newly sorted ArrayList
+ * 
+ * Utilize a Comparator from the Comparable Class to sort the Arraylist from another field.
+ * 
+ * Print out the newly sorted ARrayList
+ */
+public class SortMyObject {
+    
+}
